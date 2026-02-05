f9d754693bda13286efbded40bc63ba196d5d9e4
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2026-02-05 14:35:09 +0530

    Create SauceDemoBaseClass.java

---
+++ src/test/java/base/SauceDemoBaseClass.java
@@ -0,0 +1,29 @@
+package base;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import utilis.UtilityClass;
+
+
+public class SauceDemoBaseClass {
+	protected WebDriver driver;
+	
+	@BeforeClass
+	public void initalSetup() throws IOException {
+		driver = new FirefoxDriver();
+		driver.get(UtilityClass.readDataFromPropertiesFile("url"));
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass
+	public void teardown() {
+		if(driver != null) {
+			driver.quit();
+		}
+	}
+}
