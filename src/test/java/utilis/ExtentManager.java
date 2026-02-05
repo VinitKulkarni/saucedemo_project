7cd62c4186a7ef52af658c92cc4bf20b752fe64a
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2026-02-05 14:43:21 +0530

    Create ExtentManager.java

---
+++ src/test/java/utilis/ExtentManager.java
@@ -0,0 +1,26 @@
+package utilis;
+
+import java.io.File;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+	private static ExtentReports extent;
+	
+	
+	public static ExtentReports getReport() {
+
+        if (extent == null) {
+
+            String reportPath = System.getProperty("user.dir")
+                    + File.separator + "reports"
+                    + File.separator + "extent-report.html";
+
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+        }
+        return extent;
+    }
+}
